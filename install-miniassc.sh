dcce2cabf97896d67efb7c62e9213d06902a3727
Boris <sabredeboris@gmail.com>
2026-03-25 19:18:03 +0100

    ajout "miniassc update" + updt ext vsc dynamique

--- install-miniassc.sh
+++ install-miniassc.sh
@@ -12,8 +12,19 @@ cd
 fi
 chmod +x miniassc
 cp miniassc $HOME/.local/bin
-if [[ ! -d "$HOME/.local/share/script_assets" ]];then mkdir $HOME/.local/share/script_assets; fi
+if [[ ! -d "$HOME/.local/share/script_assets" ]];
+then mkdir $HOME/.local/share/script_assets; fi
 cp main.py $HOME/.local/share/script_assets
+cp $0 $HOME/.local/share/script_assets
 
-# installing vsc extension
-code --install-extension ./*.vsix
+# uninstalling previous extension
+code --list-extensions | grep "miniassembly" | while read extension;
+do
+    #echo $extension
+    code --uninstall-extension $extension --force
+done
+
+# installing vsc extension latest version
+file=$(curl -s https://api.github.com/repos/Terre456/MiniAssembly-vsc-extension/contents/ | jq -r '.[] | select(.name | contains(".vsix")).name'| sort -Vr | head -1)
+curl -L -O "https://raw.githubusercontent.com/Terre456/MiniAssembly-vsc-extension/master/$file"
+code --install-extension $file
